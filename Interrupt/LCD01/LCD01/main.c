5740a68d966d23500ae247b1cb60663b9c1e06b3
sunmi <ksm8430058@naver.com>
2022-06-20 14:59:27 +0900

    인터럭트& LCD 동작제어

---
+++ Interrupt/LCD01/LCD01/main.c
@@ -0,0 +1,18 @@
+/*
+ * LCD01.c
+ *
+ * Created: 2022-06-20 오후 2:53:04
+ * Author : PKNU
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
